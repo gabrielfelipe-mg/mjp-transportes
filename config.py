026a242323b36212e0c094517c77e62682b50fb7
gabrielfelipe-mg <gabrielfps76@gmail.com>
2026-09-03 10:55:05 -0300

    security: refatora conexao com banco para usar variaveis de ambiente

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
-SECRET_KEY='mjp_transportes'
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-    SGBD='mysql+mysqlconnector',
-    usuario='root',
-    senha='gabrielfps123',
-    servidor='localhost',
-    database='viagens'
-)
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+SGBD = os.getenv('DB_SGBD')
+usuario = os.getenv('DB_USER')
+senha = os.getenv('DB_PASSWORD')
+servidor = os.getenv('DB_HOST')
+database = os.getenv('DB_NAME')
+
+SQLALCHEMY_DATABASE_URI = f"{SGBD}://{usuario}:{senha}@{servidor}/{database}"
